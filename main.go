b435f0617e9fd0d07ba5e69b0a1845afb788904a
Yu-Lang Chu <yulang.chu@gmail.com>
2021-10-01 00:04:57 +0200

    add log

--- main.go
+++ main.go
@@ -5,27 +5,48 @@ import (
 	"os"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 )
 
 const VERSION = "VERSION"
 
+var log = logrus.New()
+var version = os.Getenv(VERSION)
+
 func main() {
+	if version == "" {
+		version = "0.0.0"
+	}
+
+	log.Out = os.Stdout
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		PadLevelText:  false,
+		//DisableColors: true,
+	},
+	)
+
 	r := httprouter.New()
 	r.GET("/", indexHandler)
 	r.GET("/healthz", func(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 		w.WriteHeader(http.StatusOK)
 	})
+
+	port := ":8000"
+	log.Println("listening to port", port)
+	log.Fatal(http.ListenAndServe(port, r))
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	for k, vals := range r.Header {
 		for _, val := range vals {
 			w.Header().Add(k, val)
 		}
 	}
-	ver := os.Getenv(VERSION)
-	if ver == "" {
-		ver = "0.0.0"
-	}
-	w.Header().Add(VERSION, ver)
+	log.WithFields(logrus.Fields{
+		"ip":     r.RemoteAddr,
+		"status": http.StatusOK,
+	}).Info("user:")
+	w.Header().Add(VERSION, version)
+	w.WriteHeader(http.StatusOK)
 }
